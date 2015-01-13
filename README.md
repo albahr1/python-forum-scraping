4c95ce7c6f7da6688cdbb46834cfecfd4f4507e6
Michael Seydel <micseydel@gmail.com>
2015-01-12 23:48:38 -0800

    First commit. Contains some barely-getting-started code.

---
+++ README.md
@@ -0,0 +1,7 @@
+Fragile scraper for the phpBB forum python-forum.org. At the moment it simply scrapes the banned IP addresses, then reports the total banned, perecent of IPv4 addresses banned, and duplicate ban entries as well as makes the banned/dupe lists available in the global scope (nice for a python -i session).
+
+Example output:
+Total banned IPs: 1798877
+Percent of IPv4 addresses: 0.042%
+Total duplicate entries: 257
+
---
+++ pythonforum_cleaner.py
@@ -0,0 +1,53 @@
+import json
+from itertools import chain
+
+from mechanize import Browser
+
+LOGIN_FILE = "login.json"
+
+LOGIN_URL = "http://python-forum.org/ucp.php?mode=login"
+IP_BAN_URL = "http://python-forum.org/mcp.php?i=ban&mode=ip"
+
+def main():
+    br = Browser()
+    br.open(LOGIN_URL)
+    br.select_form(nr=1)
+
+    with open(LOGIN_FILE) as login_file:
+        credentials = json.load(login_file)
+        br["username"] = credentials["username"]
+        br["password"] = credentials["password"]
+
+    br.submit()
+
+    br.open(IP_BAN_URL)
+
+    items = list(br.forms())[1].controls[-3].get_items()
+    ip_addrs = [item.attrs["contents"] for item in items]
+
+    banned = set()
+    dupes = set()
+    for addr in chain(*map(expand, ip_addrs)):
+        if addr in banned:
+            dupes.add(addr)
+        else:
+            banned.add(addr)
+
+    return (banned, dupes)
+
+def expand(pat):
+    "expand an IP address string with wild cards, e.g. 10.0.0.* -> [10.0.0.0, 10.0.0.1, ..., 10.0.0.255]"
+    if '*' not in pat:
+        yield pat
+    else:
+        template = pat.replace("*", "{}", 1)
+        for x in xrange(256):
+            for expanded in expand(template.format(x)):
+                yield expanded
+
+if __name__ == "__main__":
+    banned, dupes = main()
+
+    print 'Total banned IPs:', len(banned)
+    print 'Percent of IPv4 addresses: {:.3f}%'.format(len(banned) * 100.0 / 2**32)
+    print 'Total duplicate entries:', len(dupes)
